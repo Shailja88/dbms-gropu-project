ecf81668815683dc591a1d8822a0dbd91f3d399a
Shailja <genieshailja@gmail.com>
2023-04-04 03:17:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -66,20 +66,27 @@ data.forEach(element => {
   });
 
   //search filter{}
-// search_input.addEventListener('keyup',()=>{
-//     let filter = search_input.value.toUpperCase();
-//     let a=search.getElementsByTagName('a');
+search_input.addEventListener('keyup',()=>{
+    let filter = search_input.value.toUpperCase();
+    let a=search.getElementsByTagName('a');
 
-//     for(let index=0;index<a.length;index++){
-//      let b=a[index].getElementsByClassName('cont')[0];
-//     //  console.log(a.textContent);
-//     let TextValue= b.textContent || b.innerText;
-//     if(TextValue.toUpperCase().indexOf(filter)>-1){
-//    a[index].style.display="flex";
-//    search.style.visibility="visible";
-//    search.style.opacity=1;
-//     }
-    
-//     }
-//   })
+    for(let index=0;index<a.length;index++){
+     let b=a[index].getElementsByClassName('cont')[0];
+    //  console.log(a.textContent);
+    let TextValue = b.textContent || b.innerText;
+    if(TextValue.toUpperCase().indexOf(filter) > -1){
+   a[index].style.display="flex";
+   search.style.visibility="visible";
+   search.style.opacity=1;
+    }
+    else{
+        a[index].style.display="none";
+    }
+    if(search_input.value==0){
+        search.style.visibility="hidden";
+        search.style.opacity=0;
+      }
+      
+    }
+  })
 });
